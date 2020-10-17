35735a184615f41acf6f847d29fc12276bb3f000
maduoma <18477889+maduoma@users.noreply.github.com>
2020-10-17 18:01:19 +0100

    Driver

--- Driver/app/src/main/java/com/dodemy/driver/Interfaces/IFCMService.java
+++ Driver/app/src/main/java/com/dodemy/driver/Interfaces/IFCMService.java
@@ -9,7 +9,6 @@ import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface IFCMService {
-    ////AAAA4zxaLrc:APA91bH_nRR08YI7Xi8rFMlzAWTR9d5FBmEJR4h-RZ8a_yo2EbmRFqEAlcRID6DjpVy4zIWSwhhVmvsD-jVPZrkuaoifMldgxmSZ5OKzBzSMcIRI0iyD74DSwo36vEsHiwTCXg2i4GUa3YcUYK0137uaTfzIPzjzHw
     @Headers({
         "Content-Type:application/json",
         "Authorization:key=AAAAD038GiQ:APA91bHioSxKOfxXazizlawjl1h6W6OUbxHHKO9wDYOk4qyWHC8kREjeAU0U3v8BeDgTUO5Kark--tEKT4mrFMQxpg7urRPXAbJIUrmC1WxmlP-eFhmd2s5aoP_7oHzwQ7LY0ZVV1dgQ"
